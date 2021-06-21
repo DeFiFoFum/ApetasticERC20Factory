c0bb5298dfa35a466afc60af9967952fd3f714de
defifofum <defifofum0@gmail.com>
2021-06-21 13:25:18 -0400

    config: Update configuration

--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
 require('dotenv').config();
 
 const BSC_DEPLOYER_KEY = process.env.BSC_DEPLOYER_KEY;
@@ -62,7 +62,7 @@ module.exports = {
   compilers: {
     solc: {
       //https://forum.openzeppelin.com/t/how-to-deploy-uniswapv2-on-ganache/3885
-      version: "0.8.3",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
