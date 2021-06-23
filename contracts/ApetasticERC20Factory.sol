1a29901fbb394b52f0ce7f0dff332764b3d8d76f
defifofum <defifofum0@gmail.com>
2021-06-23 15:54:08 -0400

    feat: Update factory contract to hold a beneficiary address

--- contracts/ApetasticERC20Factory.sol
+++ contracts/ApetasticERC20Factory.sol
@@ -16,21 +16,27 @@ import "./ERC20/ERC20Initializable.sol";
 import "./ERC20/SafeERC20.sol";
 import "./utils/Ownable.sol";
 
-
+/// @title A smart contract for generating ERC-20 tokens
+/// @author Apetastic
 contract ApetasticERC20Factory is Ownable {
     using SafeERC20 for IERC20;
     bytes32 public constant INIT_CODE_TOKEN_HASH = keccak256(abi.encodePacked(type(ERC20Initializable).creationCode));
 
+    address public beneficiary;
     address[] public allTokens;
 
     event TokenCreated(address indexed tokenAddress, uint256 startingSupply);
+    event TransferBeneficiary(address indexed oldBeneficiary, address indexed beneficiary);
+    event SweepToken(IERC20 indexed token, address indexed beneficiary, uint256 balance);
 
-    /// @dev Return the number of tokens created by this contract
+
+    /// @notice Return the number of tokens created by this contract
     function allTokensLength() external view returns (uint) {
         return allTokens.length;
     }
 
-    /// @dev Create a new ERC-20 contract
+    /// @notice Create a new ERC-20 contract
+    /// @dev Tokens created have 18 decimals which means add 18 zeros the integer supply: 000000000000000000 
     /// @param name The name of the token
     /// @param symbol The symbol of the token
     /// @param supply The totalSupply of the token
@@ -42,11 +48,21 @@ contract ApetasticERC20Factory is Ownable {
         return address(token);
     }
 
-    /// @notice A public function to sweep accidental ERC20 transfers to this contract. 
+    /// @notice Change the beneficiary who recieves swept tokens
+    /// @param newBeneficiary The address of the new beneficiary
+    function setBeneficiary(address newBeneficiary) external onlyOwner {
+        address oldBeneficiary = beneficiary;
+        beneficiary = newBeneficiary;
+        emit TransferBeneficiary(oldBeneficiary, beneficiary);
+    }
+
+    /// @notice An external function to sweep ERC20 tokens locked in this contract
     ///   Tokens are sent to owner
+    /// @dev Only the owner can call this function, but the tokens are transferred to the beneficiary address
     /// @param token The address of the ERC20 token to sweep
-    function sweepToken(IERC20 token) external onlyOwner {
+    function sweepToken(IERC20 token) external {
         uint256 balance = token.balanceOf(address(this));
-        token.safeTransfer(msg.sender, balance);
+        token.safeTransfer(beneficiary, balance);
+        emit SweepToken(token, beneficiary, balance);
     }
 }
